101cc4b7c56698599b52c37d690329b2854c25f3
Rredlik <rredlik@mail.ru>
2022-11-23 16:53:39 +0500

    Voice and listen update

---
+++ listener.py
@@ -0,0 +1,31 @@
+import vosk
+import sys
+import sounddevice as sd
+import queue
+
+model = vosk.Model('model_small')
+samplerate = 16000
+device = 1
+
+q = queue.Queue()
+
+
+def callback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    q.put(bytes(indata))
+
+
+with sd.RawInputStream(samplerate=samplerate,
+                       blocksize=8000,
+                       device=device,
+                       dtype='int16',
+                       channels=1,
+                       callback=callback):
+    rec = vosk.KaldiRecognizer(model, samplerate)
+    while True:
+        data = q.get()
+        if rec.AcceptWaveform(data):
+            print(rec.Result())
+        else:
+            print(rec.PartialResult())
--- speaker.py
+++ speaker.py
@@ -11,7 +11,6 @@ speaker = 'baya'  # aidar, baya, kseniya, xenia, random
 put_accent = True
 put_yo = True
 device = torch.device('cpu')  # or gpu
-# from sound
 
 
 model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
@@ -20,17 +19,6 @@ model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
                           speaker=model_id)
 model.to(device)
 
-
-# print(text)
-# audio = model.apply_tts(text=text,
-#                         speaker=speaker,
-#                         sample_rate=sample_rate,
-#                         put_accent=put_accent,
-#                         put_yo=put_yo)
-# sd.play(audio, sample_rate)
-# time.sleep(len(audio) / sample_rate*1.01)
-# print(sample_rate)
-# sd.stop()
 
 def speak(text):
     _audio = model.apply_tts(text=text,
@@ -41,21 +29,3 @@ def speak(text):
     sd.play(_audio, sample_rate)
     time.sleep(len(_audio) / sample_rate)
     sd.stop()
-
-
-# engine = pyttsx3.init()
-# voices = engine.getProperty('voices')
-# # print(voices)
-# engine.setProperty('voices', voices[0].id)
-# engine.setProperty('rate', 250)
-#
-# volume = engine.getProperty('volume')  # getting to know current volume level (min=0 and max=1)
-
-# def speak(audio):
-#     # current_volume = Sound.current_volume
-#     engine.say(audio)
-#     engine.runAndWait()
-
-# if __name__ == '__main__':
-#     # speak('Привет! Как твои дела, броо')
-#     robo_speak("Абоба")
