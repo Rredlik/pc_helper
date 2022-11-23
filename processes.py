d8af55799b33f99c6b6c8be3cbf843709159adda
Rredlik <rredlik@mail.ru>
2022-11-23 12:17:32 +0500

    Update processes.py

--- processes.py
+++ processes.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+import subprocess
 
 import wmi
 PRIORITY_PROGRAMS = ['Calculator.exe']
@@ -12,6 +14,21 @@ async def processes():
             print(f'{process.ProcessID:<10} {process.Name}')
 
 
+def start_browser(close_program):
+    programs_in_work = []
+    try:
+        _procces = subprocess.check_output("tasklist /fo csv /nh")
+        _procces = str(_procces).split('\\r\\n')
+
+        for el in _procces:
+            element = el.split(',')[0].replace('"', '')
+            programs_in_work.append(element)
+            if element == close_program:
+                print(f"Закрываю программу {close_program}")
+
+    except:
+
+        print('Error')
 
 
 # async def processes():
@@ -26,9 +43,12 @@ async def processes():
 
 if __name__ == '__main__':
     # asyncio.run(processes())
-    print(f)
+    # print(start_browser)
     # for my_computer in f:
     #     print(my_computer)
+    prog = input()
+    start_browser(prog)
 
 
 
+'tasklist /v /fo list /nh > Процессы.csv'
